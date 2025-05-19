cc1d9cb26ddda0335ab8d707a69a6f61f618facf
Samuel SIlva <samuel343silva@hotmail.com>
2025-05-19 19:09:02 -0300

    ex 06 a 12 - 19/05

---
+++ Desafios/src/EstruturaDeDecisao/Challenger006.java
@@ -0,0 +1,4 @@
+package EstruturaDeDecisao;
+
+public class Challenger006 {
+}
---
+++ Desafios/src/EstruturaDeDecisao/Challenger011.java
@@ -0,0 +1,10 @@
+package EstruturaDeDecisao;
+
+import java.util.Scanner;
+
+public class Challgenger011 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
